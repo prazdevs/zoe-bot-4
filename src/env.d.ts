aef9968894594754889f84156bbdc951bf36c2e3
PraZ <sachabouillez@gmail.com>
2020-08-21 01:46:56 +0200

    :label: add more env typing

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,5 +2,9 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     DISCORD_TOKEN: string;
     POSTGRES_URL: string;
+    REDDIT_USERNAME: string;
+    REDDIT_PASSWORD: string;
+    REDDIT_APP_ID: string;
+    REDDIT_APP_SECRET: string;
   }
 }
